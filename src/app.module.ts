8d9a254089bb50c4da6e8b089c1cb5bd93d5c2c3
hangju0610 <phj950610@naver.com>
2024-01-19 00:37:30 +0900

    Feat: import ConfigModule ConfigModule을 사용하기 위해 AppConfig에 import

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,13 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import ormConfig from './config/orm.config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [ormConfig],
+    }),
+  ],
 })
 export class AppModule {}
