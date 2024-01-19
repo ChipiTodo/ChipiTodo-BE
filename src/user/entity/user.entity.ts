cd15888fe3147d2c39dc593ce7da7151346be2d1
hangju0610 <phj950610@naver.com>
2024-01-19 22:26:21 +0900

    Feat: Add User Entity

---
+++ src/user/entity/user.entity.ts
@@ -0,0 +1,28 @@
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+@Entity({ name: 'user' })
+export class User {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column()
+  email: string;
+
+  @Column({ nullable: true }) // OAuth를 위해 비밀번호 null 가능
+  password?: string;
+
+  @Column()
+  nickname: string;
+
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @UpdateDateColumn()
+  updatedAt: Date;
+}
