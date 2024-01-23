98c43455d31fba5ffe633d68d47cabbbd6df6913
Park HJ <phj950610@epopcon.com>
2024-01-23 23:13:40 +0900

    Feat: Add findByEmail Method validation을 위한 이메일 메서드작성

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,22 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { User } from './entity/user.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+  constructor(
+    @InjectRepository(User) private readonly userRepository: Repository<User>,
+  ) {}
+
+  /**
+   * email을 통해 DB의 user를 찾습니다.
+   * @param email 기본적인 email
+   * @returns {User}
+   */
+  async findByEmail(email: string): Promise<User> {
+    return await this.userRepository.findOne({ where: { email } });
+  }
+
+  async createUser(): Promise<void> {}
+}
--- test/unit/user/user.service.spec.ts
+++ test/unit/user/user.service.spec.ts
@@ -1,12 +1,22 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UserService } from '../../../src/user/user.service';
+import { User } from 'src/user/entity/user.entity';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
 
 describe('UserService', () => {
   let service: UserService;
+  type MockRepository<T = any> = Partial<Record<keyof T, jest.Mock>>;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [UserService],
+      providers: [
+        UserService,
+        {
+          provide: getRepositoryToken(User),
+          useClass: Repository,
+        },
+      ],
     }).compile();
 
     service = module.get<UserService>(UserService);
@@ -14,5 +24,15 @@ describe('UserService', () => {
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  describe('findByEmail', () => {
+    it('해당하는 email이 존재하는 경우', async () => {
+      const spy = jest.spyOn(service, 'findByEmail');
+      const user = service.findByEmail('test@naver.com');
+
+      expect(spy).toHaveBeenCalled();
+      expect(user).toBeInstanceOf(User);
+    });
   });
 });
