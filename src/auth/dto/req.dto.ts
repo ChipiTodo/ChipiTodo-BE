a52464a80dfcf55608ae70bb9daedbc0efce18ce
Park HJ <phj950610@epopcon.com>
2024-01-26 22:53:18 +0900

    Feat: Add SignupReqDto validation

--- src/auth/dto/req.dto.ts
+++ src/auth/dto/req.dto.ts
@@ -1,7 +1,21 @@
+import { IsEmail, IsNotEmpty, Length, Matches } from 'class-validator';
+
 class SignupReqDto {
+  @IsEmail()
   email: string;
+
+  @IsNotEmpty()
+  @Length(9, 20)
+  @Matches(/^(?=.*[a-zA-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]+$/, {
+    message:
+      '비밀번호에는 영문, 숫자, 특수문자가 포함되어야 하며, 특수문자는 최소 한개 이상이어야 합니다.',
+  })
   password: string;
+
+  @IsNotEmpty()
   confirmPassword: string;
+
+  @IsNotEmpty()
   nickname: string;
 }
 
