dc73dbc6c24cb28a91717ec5ef2effec4eeb1ae6
muyeon <gmakin36@gmail.com>
2024-02-02 21:01:40 +0900

    Feat: Add update space

--- src/space/space.controller.ts
+++ src/space/space.controller.ts
@@ -1,4 +1,12 @@
-import { Body, Controller, Post, Get } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Post,
+  Get,
+  Put,
+  Param,
+  ParseIntPipe,
+} from '@nestjs/common';
 import { SpaceService } from './space.service';
 import { SpaceRequestDto } from './dto/space-request.dto';
 import { Space } from './entity/space.entity';
@@ -15,5 +23,13 @@ export class SpaceController {
   @Get()
   async getAllSpace(): Promise<Space[]> | null {
     return await this.spaceService.getAllSpace();
+  }
+
+  @Put(':id')
+  async updateSpace(
+    @Param('id', ParseIntPipe) id: number,
+    @Body() spaceRequestDto: SpaceRequestDto,
+  ): Promise<Object> {
+    return this.spaceService.updateSpace(id, spaceRequestDto);
   }
 }
--- src/space/space.service.ts
+++ src/space/space.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Space } from './entity/space.entity';
 import { Repository } from 'typeorm';
@@ -25,5 +25,31 @@ export class SpaceService {
     const spaces = await this.spaceRepository.find();
 
     return spaces;
+  }
+
+  async updateSpace(id: number, body: SpaceRequestDto): Promise<Object> | null {
+    const { name, description } = body;
+
+    const space = await this.spaceRepository.findOne({
+      where: {
+        id,
+      },
+    });
+
+    if (!space) {
+      throw new NotFoundException();
+    }
+
+    if (name) {
+      space.name = name;
+    }
+
+    if (description) {
+      space.description = description;
+    }
+
+    await this.spaceRepository.save(space);
+
+    return { statusCode: 200, message: '수정 완료!' };
   }
 }
